8fc1ae413ad7d966860b7170176dedb908ab9b81
K.Srinivasan <ksrinivasan@krishna-iresearch.org>
2025-06-24 22:22:54 +0530

    1622. (THEORY and FEATURE) Computational Geometric Factorization - High Performance Computing implementation for Supercomputers - factors of multiple consecutive integers and Greastest Common Divisor computation from intersection of HashSets of factors in a Vector - related to 668,1383,1384,1385,1519,1593 and all sections on Integer Factorization,Computational Geometry,Set Partitions-to-Tile Cover reduction by Lagranges Four Square Theorem and ILP,Primality Testing,RSA Semiprimes,Almost-Square Bounding Boxes in Large Scale Visual Recognition,Pell's Equation,Brahmagupta's Chakravala,Approximate Factoring by Newton-Raphson Rayshooting queries,Algebraic Geometry of Computational Geometric Factorization,Quantum computation and Derandomization of BQP Factorization by Condorcet Jury Theorem Majority-BQPVoterSAT boolean composition - 24 June 2025

--- course_material/Programming/Rust/rasterization/src/main.rs
+++ course_material/Programming/Rust/rasterization/src/main.rs
@@ -4,24 +4,53 @@ use std::time::*;
 use rayon::prelude::*;
 use foreach::*;
 use std::collections::HashSet;
+use std::collections::HashMap;
 use std::iter::FromIterator;
 
 static mut factors: Vec<i64> = Vec::new();
+static mut number2factors: Vec<HashSet<i64>> = Vec::new();
 
 fn main() {
     let args: Vec<String>=env::args().collect();
     let number_to_factorize: i64=args[1].parse().unwrap(); 
     let range: i64=args[2].parse().unwrap();
     let rasterization: String=args[3].clone();
-    factorize_multipleintegers(number_to_factorize,range,rasterization);
+    factorize_multipleintegers(number_to_factorize,range,rasterization.clone());
     unsafe {
-        //println!("main():Factors of {number_to_factorize} are {:#?} ",factors);
-        let factorsset: HashSet<i64> = HashSet::from_iter(factors.clone());
-        println!("main():Set of Factors of {number_to_factorize} are {:#?} ",factorsset);
+        println!("main():Vector of HashSet of Factors in integer range {number_to_factorize} to {number_to_factorize} + {range} are {:#?} ",number2factors);
     }
+    greatest_common_divisor();
+    unsafe {
+        number2factors.clear();
+    }
+    let number1: i64 = args[4].parse().unwrap();
+    let number2: i64 = args[5].parse().unwrap();
+    rasterize_hyperbolic_arc(number1,rasterization.clone());
+    rasterize_hyperbolic_arc(number2,rasterization.clone());
+    greatest_common_divisor();
     stdout().flush().unwrap();
     //pariter64bits();
 }
+
+fn greatest_common_divisor() {
+    println!("------ greatest_common_divisor() of the integer range ------- ");
+    unsafe {
+        let mut prev: HashSet<i64> = number2factors[0].clone(); 
+        let mut intersect_sorted;
+        for n in 1..number2factors.len() {
+            let mut next: HashSet<i64> = number2factors[n].clone(); 
+            let mut intersect = prev.intersection(&next);
+            intersect_sorted = intersect.into_iter().collect::<Vec<_>>();
+            intersect_sorted.sort();
+            println!("prev: {:#?}",prev);
+            println!("next: {:#?}",next);
+            println!("intersection sorted: {:#?} ", intersect_sorted);
+            println!("Greatest Common Divisor: {:#?} ",intersect_sorted[intersect_sorted.len()-1]);
+            prev = number2factors[n].clone();
+            //println!("next: {:#?} ",next);
+        }
+    }
+} 
 
 fn pariter64bits() {
     let mut len : i64 = 10000;
@@ -40,6 +69,9 @@ fn factorize_multipleintegers(num_fact:i64,range:i64,rasterization:String) {
         let mut raster = rasterization.clone();
         println!("Factorization of {number_to_factorize} began in (nanoseconds): {:#?} ",systemtimebegin);
         rasterize_hyperbolic_arc(number_to_factorize,raster);
+        unsafe {
+            factors.clear();
+        }
         let systemtimeend = SystemTime::now();
         let duration = systemtimeend.duration_since(systemtimebegin).unwrap(); 
         println!("Factorization of {number_to_factorize} was completed in (nanoseconds): {:#?} ",duration);
@@ -92,6 +124,14 @@ fn rasterize_hyperbolic_arc(num_fact: i64,rasterization: String) -> i64
         let systemtimeend = SystemTime::now();
         let duration = systemtimeend.duration_since(systemtimebegin).unwrap(); 
         println!("Parallel Rasterization phase of factoring {num_fact} was completed in (nanoseconds): {:#?} ",duration);
+        unsafe {
+            let factorsset: HashSet<i64> = HashSet::from_iter(factors.clone());
+            //println!("main():Set of Factors of {num_fact} are {:#?} ",factorsset);
+            number2factors.push(factorsset.clone());
+        }
+        unsafe {
+            factors.clear();
+        }
         stdout().flush().unwrap();
     }
     return 1;
