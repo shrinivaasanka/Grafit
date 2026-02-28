be02e9426548f0e5d6c5f12d4cd47b55627ea434
Srinivasan Kannan <ksrinivasan@krishna-iresearch.org>
2026-02-28 16:04:36 +0530

    1652. (THEORY and FEATURE) Computational Geometric Factorization - High Performance Computing implementation for Supercomputers - Rust graphics plot - On ChatGPT AI review of computational geometric factorization and factors of multiple consecutive integers from 200000 to 300000 - related to 668,1383,1384,1385,1519,1593,1622,1647 and all sections on Integer Factorization,Computational Geometry,Set Partitions-to-Tile Cover reduction by Lagranges Four Square Theorem and ILP,Primality Testing,RSA Semiprimes,Almost-Square Bounding Boxes in Large Scale Visual Recognition,Pell's Equation,Brahmagupta's Chakravala,Approximate Factoring by Newton-Raphson Rayshooting queries,Algebraic Geometry of Computational Geometric Factorization,Quantum computation and Derandomization of BQP Factorization by Condorcet Jury Theorem Majority-BQPVoterSAT boolean composition - 27 February 2026

--- course_material/Programming/Rust/rasterization/src/main.rs
+++ course_material/Programming/Rust/rasterization/src/main.rs
@@ -18,7 +18,7 @@ fn main() {
     let rasterization: String=args[3].clone();
     factorize_multipleintegers(number_to_factorize,range,rasterization.clone());
     unsafe {
-        println!("main():Vector of HashSet of Factors in integer range {number_to_factorize} to {number_to_factorize} + {range} are {:#?} ",number2factors);
+        //println!("main():Vector of HashSet of Factors in integer range {number_to_factorize} to {number_to_factorize} + {range} are {:#?} ",number2factors);
     }
     greatest_common_divisor();
     unsafe {
@@ -34,7 +34,7 @@ fn main() {
 }
 
 fn greatest_common_divisor() {
-    println!("------ greatest_common_divisor() of the integer range ------- ");
+    //println!("------ greatest_common_divisor() of the integer range ------- ");
     unsafe {
         let mut prev: HashSet<i64> = number2factors[0].clone(); 
         let mut intersect_sorted;
@@ -43,10 +43,10 @@ fn greatest_common_divisor() {
             let mut intersect = prev.intersection(&next);
             intersect_sorted = intersect.into_iter().collect::<Vec<_>>();
             intersect_sorted.sort();
-            println!("prev: {:#?}",prev);
-            println!("next: {:#?}",next);
-            println!("intersection sorted: {:#?} ", intersect_sorted);
-            println!("Greatest Common Divisor: {:#?} ",intersect_sorted[intersect_sorted.len()-1]);
+            //println!("prev: {:#?}",prev);
+            //println!("next: {:#?}",next);
+            //println!("intersection sorted: {:#?} ", intersect_sorted);
+            //println!("Greatest Common Divisor: {:#?} ",intersect_sorted[intersect_sorted.len()-1]);
             prev = number2factors[n].clone();
             //println!("next: {:#?} ",next);
         }
@@ -57,7 +57,7 @@ fn pariter64bits() {
     let mut len : i64 = 10000;
     let mut vector = 1 .. len;
     vector.par_bridge().for_each(|paritem| {
-        println!("item {:#?} :",paritem);
+        //println!("item {:#?} :",paritem);
     });
 }
 
@@ -68,7 +68,7 @@ fn factorize_multipleintegers(num_fact:i64,range:i64,rasterization:String) {
         let mut systemtimebegin = SystemTime::now();
         let mut number_to_factorize = num_fact+i;
         let mut raster = rasterization.clone();
-        println!("Factorization of {number_to_factorize} began in (nanoseconds): {:#?} ",systemtimebegin);
+        //println!("Factorization of {number_to_factorize} began in (nanoseconds): {:#?} ",systemtimebegin);
         rasterize_hyperbolic_arc(number_to_factorize,raster);
         unsafe {
             factors.clear();
@@ -81,7 +81,7 @@ fn factorize_multipleintegers(num_fact:i64,range:i64,rasterization:String) {
 	}
     }
     unsafe {
-    	println!("Range Factorization durations: {:#?} ",durations);
+    	//println!("Range Factorization durations: {:#?} ",durations);
 	plot!("Factorization runtime plot", durations);
     }
 }
@@ -97,19 +97,19 @@ fn rasterize_hyperbolic_arc(num_fact: i64,rasterization: String) -> i64
         y1.foreach(|item,iter| {
             let mut xtile_start = num_fact/item;
             let mut xtile_end = num_fact/(item+1);
-            println!("tile segment {item}: from {xtile_start} to {xtile_end}");
+            //println!("tile segment {item}: from {xtile_start} to {xtile_end}");
             binary_search(num_fact,xtile_end,item,xtile_start,item);
         });
         let systemtimeend = SystemTime::now();
         let duration = systemtimeend.duration_since(systemtimebegin).unwrap(); 
-        println!("Sequential Rasterization phase of factoring {num_fact} was completed in (nanoseconds): {:#?} ",duration);
+        //println!("Sequential Rasterization phase of factoring {num_fact} was completed in (nanoseconds): {:#?} ",duration);
         stdout().flush().unwrap();
     }
     if rasterization=="parallel"
     {
         let systemtimebegin = SystemTime::now();
         let numberofbits = num_fact.ilog2();
-        println!("Size of integer to be factorized (in bits): {numberofbits}");
+        //println!("Size of integer to be factorized (in bits): {numberofbits}");
         if numberofbits <= 32 
         {
             y2.into_par_iter().for_each(|paritem| {
@@ -130,10 +130,11 @@ fn rasterize_hyperbolic_arc(num_fact: i64,rasterization: String) -> i64
         }
         let systemtimeend = SystemTime::now();
         let duration = systemtimeend.duration_since(systemtimebegin).unwrap(); 
-        println!("Parallel Rasterization phase of factoring {num_fact} was completed in (nanoseconds): {:#?} ",duration);
+        //println!("Parallel Rasterization phase of factoring {num_fact} was completed in (nanoseconds): {:#?} ",duration);
         unsafe {
             let factorsset: HashSet<i64> = HashSet::from_iter(factors.clone());
             //println!("main():Set of Factors of {num_fact} are {:#?} ",factorsset);
+            println!("main():Set of Factors of {num_fact} are {:#?} ",factors.clone());
             number2factors.push(factorsset.clone());
         }
         unsafe {
@@ -172,7 +173,7 @@ fn binary_search(num_fact:i64,xl:i64,yl:i64,xr:i64,yr:i64)
         if factorcandidate==num_fact
         {
              let systemtimenow = SystemTime::now();
-             println!("Factor point located : {midpoint} , {yl_clone} at {:#?} nanoseconds",systemtimenow);
+             //println!("Factor point located : {midpoint} , {yl_clone} at {:#?} nanoseconds",systemtimenow);
              unsafe {
                 factors.push(factorcandidate);
                 //println!("2.Factors of {num_fact} are {:#?} ",factors);
@@ -181,7 +182,7 @@ fn binary_search(num_fact:i64,xl:i64,yl:i64,xr:i64,yr:i64)
         if xl_clone*yl_clone==num_fact
         {
              let systemtimenow = SystemTime::now();
-             println!("Factor point located : {xl_clone} , {yl_clone} at {:#?} nanoseconds",systemtimenow);
+             //println!("Factor point located : {xl_clone} , {yl_clone} at {:#?} nanoseconds",systemtimenow);
              unsafe {
                 factors.push(xl_clone);
                 factors.push(yl_clone);
@@ -192,7 +193,7 @@ fn binary_search(num_fact:i64,xl:i64,yl:i64,xr:i64,yr:i64)
         {
 
              let systemtimenow = SystemTime::now();
-             println!("Factor point located : {xr_clone} , {yr_clone} at {:#?} nanoseconds",systemtimenow);
+             //println!("Factor point located : {xr_clone} , {yr_clone} at {:#?} nanoseconds",systemtimenow);
              unsafe {
                 factors.push(xr_clone);
                 factors.push(yr_clone);
