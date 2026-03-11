36ec4c5ff75c1dd4577fd2abff0ead2c5cf030e0
Srinivasan Kannan <ka.shrinivaasan@gmail.com>
2026-03-11 14:11:50 +0530

    1654. (THEORY and FEATURE) Brute Force Factorization in C++ - Trial division - related to 668,1383,1384,1385,1519,1593,1622,1647,1652,1653 and all sections on Integer Factorization,Computational Geometry,Set Partitions-to-Tile Cover reduction by Lagranges Four Square Theorem and ILP,Primality Testing,RSA Semiprimes,Almost-Square Bounding Boxes in Large Scale Visual Recognition,Pell's Equation,Brahmagupta's Chakravala,Approximate Factoring by Newton-Raphson Rayshooting queries,Algebraic Geometry of Computational Geometric Factorization,Quantum computation and Derandomization of BQP Factorization by Condorcet Jury Theorem Majority-BQPVoterSAT boolean composition, Spark-Python and Rust Rayon computational geometric parallel planar point location factorization - 11 March 2026

--- course_material/Programming/C++/code/bruteforcefactorization.cpp
+++ course_material/Programming/C++/code/bruteforcefactorization.cpp
@@ -2,12 +2,36 @@
 #include <iostream>
 #include <array>
 #include <cmath>
+#include <chrono>
 
 using namespace std;
 
+void factorization1(int& num_to_factorize);
+void factorization2(int& num_to_factorize);
+
 int main(int argc, char* argv[])
 {
-	const int num_to_factorize = std::atoi(argv[1]);
+	int num_to_factorize = std::atoi(argv[1]);
+	cout<<"-------------------------------------------------"<<endl;
+	cout<<"1.Factorization1 - Exhaustive real plane search"<<endl;
+	cout<<"-------------------------------------------------"<<endl;
+	auto start = std::chrono::high_resolution_clock::now();
+	factorization1(num_to_factorize);
+	auto end = std::chrono::high_resolution_clock::now();
+	auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end-start).count();
+	cout<<"1.Bruteforce factorization1() - exhaustive real plane search -  runtime in nanoseconds:"<<duration<<endl;
+	cout<<"-------------------------------------------------"<<endl;
+	cout<<"2.Factorization2 - Recursive trial division"<<endl;
+	cout<<"-------------------------------------------------"<<endl;
+	start = std::chrono::high_resolution_clock::now();
+	factorization2(num_to_factorize);
+	end = std::chrono::high_resolution_clock::now();
+	duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end-start).count();
+	cout<<"2.Bruteforce factorization2() - recursive trial division - runtime in nanoseconds:"<<duration<<endl;
+}
+
+void factorization1(int& num_to_factorize)
+{
 	int exponent;
 	for(int x=1;x <= num_to_factorize;x++)
 	{
@@ -20,6 +44,20 @@ int main(int argc, char* argv[])
 			cout<<"---------------------------------------------------------------"<<endl;
 			cout<<"Factors of "<<num_to_factorize<<" are "<<factorcandidate<<" and "<<x<<endl;
 			cout<<"---------------------------------------------------------------"<<endl;
+		}
+	}
+}
+
+void factorization2(int& num_to_factorize)
+{
+	int integer=num_to_factorize;
+	for(int x=1;x <= num_to_factorize;x++)
+	{
+		int remainder = integer%x;
+		if(remainder == 0)
+		{
+			cout<<"Factor of "<<num_to_factorize<<" found:"<<x<<endl;
+			integer = integer/x;
 		}
 	}
 }
